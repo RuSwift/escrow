ceee3548b38e42f51bfc35b9121f8416e7d62e76
Pavel Minenkov <p.minenkof@arteus.cy>
2026-03-08 03:17:21 +0300

    add ledgers

---
+++ ledgers/chat/__init__.py
@@ -0,0 +1,22 @@
+"""
+Модуль для работы с чатом - модели сообщений
+"""
+from .schemas import (
+    MessageType,
+    AttachmentType,
+    MessageSignature,
+    FileAttachment,
+    ChatMessage,
+    ChatMessageCreate,
+    ChatMessageResponse,
+)
+
+__all__ = [
+    'MessageType',
+    'AttachmentType',
+    'MessageSignature',
+    'FileAttachment',
+    'ChatMessage',
+    'ChatMessageCreate',
+    'ChatMessageResponse',
+]
